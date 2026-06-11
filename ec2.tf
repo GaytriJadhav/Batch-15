4ee80319597908c34d0b0c339a6217008411ca02
GaytriJadhav <gayujadhav5424@gmail.com>
2026-06-11 17:37:38 +0530

    Add EC2 instance configuration in ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-091138d0f0d41ff90"
+  instance_type = "t3.micro"
+  key_name = "id"
+  vpc_security_group_ids = ["sg-0df6a830e151d7b6e"]
+  tags = {
+    Name = "myec2"
+    name = "gaytri"
+    env = "dev"
+  }
+}
