9f2adeec704453528a2ca61fc3c8662dfa407717
GaytriJadhav <gayujadhav5424@gmail.com>
2026-06-11 18:10:31 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "myec2" {
   ami           = "ami-091138d0f0d41ff90"
   instance_type = "t3.micro"
-  key_name = "id"
+  key_name = "terr-key"
   vpc_security_group_ids = ["sg-0df6a830e151d7b6e"]
   tags = {
     Name = "myec2"
